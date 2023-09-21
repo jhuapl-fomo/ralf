4334784607cfa329bcfc861b8652627e881c135a
Arun Reddy <arun.reddy@jhuapl.edu>
2023-09-21 09:32:09 -0400

    Version bump to 0.0.5

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Johns Hopkins University Applied Physics Laboratory'
 author = 'JHUAPL'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.4'
+release = '0.0.5'
 
 
 # -- General configuration ---------------------------------------------------
